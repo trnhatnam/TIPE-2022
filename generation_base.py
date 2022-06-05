9ae7ff24762e93d35276098784c2eeb1230d8f2f
nhatnam <55913360+risuroo@users.noreply.github.com>
2022-06-05 15:32:32 +0200

    Update generation_base.py

--- generation_base.py
+++ generation_base.py
@@ -1,12 +1,16 @@
 from PIL import Image, ImageFont, ImageDraw
 from string import ascii_uppercase
 import os
+"""
+Toutes les polices sont en format .ttf et sont dans le même dossier
+L'idée est de chercher toutes les polices (càd fichier.ttf) dans le dossier de travail (donné par os.getcwd()).
+Pour chaque police, on met en image chaque lettre et chiffre de cette police qu'on met respectivement dans un dossier pour les lettres et pour les chiffres.
+Les fichiers sont des images 25x50 nommés avec cette convention : (charactère)_(numéro).png
 
-# Toutes les polices sont en format .ttf et sont dans le même dossier
-# L'idée est de chercher toutes les polices (càd fichier.ttf) dans le dossier de travail (donné par os.getcwd()).
-# Pour chaque police, on met en image chaque lettre et chiffre de cette police qu'on met respectivement dans un dossier pour les lettres et pour les chiffres.
-# Les fichiers sont des images 25x50 nommés avec cette convention : (characères)_(numéro).png
-# Le numéro sert à différencier les polices entre elles.
+Les images seront utilisés pour construire la base des lettres et chiffres pour la reconnaissance des caractères.
+On a donc chercher des polices qui ont des similitudes avec les caractères des plaques SIV.
+"""
+
 
 
 base = ascii_uppercase + '0123456789'
